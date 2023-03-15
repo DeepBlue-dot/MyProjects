f1de231b1bf3ef0de37f84ee75eb45ee6123d7d7
DeepBlue-dot <deepblue5991@gmail.com>
2023-03-15 21:39:29 +0300

    regrate

--- snake/main.cpp
+++ snake/main.cpp
@@ -5,214 +5,59 @@
 #include <thread>
 #include <chrono>
 
-
-
-
 using namespace std;
 
-int hieght = 35, width = 100, length = 50,p =0, speed = 10, score=0;
+int hieght = 35, width = 100, length = 10,p =0, speed = 10, score=0;
 bool apple_= true , not_pause = false;
 int run = 0;
-char direction = 'r',c;
+char direction = 'r';
 
 vector<vector<int>> coo = {{10,10}};
 vector<int> apple_xy ={-1,-1};
 
+int getch(void);
+void snake(int l);
+void game_over();
+void check();
+void input();
+void move();
+void apple();
+void print();
 
-int getch(void) 
-{
-    struct termios oldattr, newattr;
-    int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
-    newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
-    return ch;
-}
 
-void snake(int l)
+int main()
 {
-    for(int i = 0; i < (l-1); i++)
+    thread th1(input);
+    snake(length);
+    srand(time(NULL));
+    do
     {
-        coo.push_back({coo[coo.size()-1][0]+1,coo[coo.size()-1][1]});
-    }
-}
-
-void game_over()
-{
-    run = 1;
-    coo.clear();
-    coo.push_back({10,10});
-    snake(length); 
-    direction = 'r';
-    apple_=true;
-    apple_xy.clear();
-    apple_xy[0] = -1;
-    apple_xy[1] = -1;
-    not_pause = false;
-
-}
-
-void check()
-{
-    for (int i=0; i < coo.size(); i++)
-    {
-        if(coo[i][0] < 1 || coo[i][0] >= width-1 || coo[i][1] < 1 || coo[i][1] >= hieght-1)
+        if (run == 0)
         {
-            game_over();
-        }
-    
-        if (coo[i][0] == apple_xy[0] && coo[i][1] == apple_xy[1])
-        {
-            apple_ = true;
-                switch (direction)
-                {
-                    case 'u':
-                        coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]-1});
-                        break;
-                    case 'd':
-                        coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]+1});
-                        break;
-                    case 'r':
-                        coo.push_back({coo[coo.size()-1][0]+1,coo[coo.size()-1][1]});
-                        
-                        break;
-                    case 'l':
-                        coo.push_back({coo[coo.size()-1][0]-1,coo[coo.size()-1][1]});
-                        break;
-                    default:
-                        break;
-                }
-            score+=1;
-        }
-        
-    }
-    for(int p =0; p < coo.size(); p++)
-    {
-        for(int q =0; q < coo.size(); q++)
-        {
-            if(p != q && coo[p][0] == coo[q][0] && coo[p][1] == coo[q][1])
+            cout << "Score: \t"<<score <<endl;
+            if(not_pause)
             {
-                game_over();
+                print();
+                move();
+                check();
+                apple();
+                
+            }
+            else
+            {
+                print();
+                
             }
         }
-
-    }
-    
-}
-
-
-
-void input()
-{
-    do {
-         switch (getch())
-            {
-                case 65:
-                    if(direction != 'd' && direction != 'u' && not_pause)
-                    {
-                        direction = 'u';
-                    }
-                    break;
-                case 66:
-                    if(direction != 'd' && direction != 'u' && not_pause)
-                    {
-                        direction = 'd';
-                    }
-                    break;
-                case 68:
-                    if(direction != 'l' && direction != 'r' && not_pause)
-                    {
-                        direction = 'l';
-                    }
-                    break;
-                case 67:
-                    if(direction != 'l' && direction != 'r' && not_pause)
-                    {
-                        direction = 'r';
-                    }
-                    break;
-                case 'v':
-                    if(run == 0)
-                    {
-                        if(not_pause)
-                        {
-                            not_pause = false;
-                        }
-                        else
-                        {
-                            not_pause = true;
-                        }
-                    }
-                    else if(run == 2)
-                    {
-                        run = 0;
-                        system("clear");
-                        score = 0;
-                    }
-                default:
-                    break;
-            }
-	} while(true);
-	 
-}
-
-void move()
-{
-    switch (direction)
-    {
-        case 'u':
-            coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]-1});
-            coo.erase(coo.begin());
-            break;
-        case 'd':
-            coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]+1});
-            coo.erase(coo.begin());
-            break;
-        case 'r':
-            coo.push_back({coo[coo.size()-1][0]+1,coo[coo.size()-1][1]});
-            coo.erase(coo.begin());
-            break;
-        case 'l':
-            coo.push_back({coo[coo.size()-1][0]-1,coo[coo.size()-1][1]});
-            coo.erase(coo.begin());
-            break;
-        default:
-            break;
-    }
-    
-}
-
-void apple()
-{
-    if(apple_)
-    {
-        bool loop = true;
-        while (loop)
+        else if(run == 1)
         {
-            apple_xy[0] = rand() % (width);
-            apple_xy[1] = rand() % (hieght);
-            for (vector e : coo)
-            {
-                if(e[0] == apple_xy[0] && e[1] == apple_xy[1])
-                {
-                    loop = true;
-                    break;
-                }
-                else
-                {
-                    if(0 < apple_xy[0]  && apple_xy[0]  < width-1 && 0 < apple_xy[1]  && apple_xy[1] < hieght-1)
-                    {
-                        loop = false;
-                    }
-                }
-            }
+            cout << "Score: \t"<< score << endl;
+            cout << "\n\t\tGAME OVER!!\n";
+            run = 2;
         }
-        apple_ = false;
-    }
-        
+
+    }while (true);
+     
 }
 
 void print()
@@ -293,39 +138,202 @@ void print()
 
 }
 
-int main()
+void apple()
 {
-    thread th1(input);
-    snake(length);
-    srand(time(NULL));
-    do
+    if(apple_)
     {
-
-        if (run == 0)
+        bool loop = true;
+        while (loop)
         {
-            cout << "Score: \t"<<score <<endl;
-            if(not_pause)
+            apple_xy[0] = rand() % (width);
+            apple_xy[1] = rand() % (hieght);
+            for (vector e : coo)
             {
-                print();
-                move();
-                check();
-                apple();
-                
-            }
-            else
-            {
-                print();
-                
+                if(e[0] == apple_xy[0] && e[1] == apple_xy[1])
+                {
+                    loop = true;
+                    break;
+                }
+                else
+                {
+                    if(0 < apple_xy[0]  && apple_xy[0]  < width-1 && 0 < apple_xy[1]  && apple_xy[1] < hieght-1)
+                    {
+                        loop = false;
+                    }
+                }
             }
         }
-        else if(run == 1)
+        apple_ = false;
+    }
+        
+}
+
+
+void move()
+{
+    switch (direction)
+    {
+        case 'u':
+            coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]-1});
+            coo.erase(coo.begin());
+            break;
+        case 'd':
+            coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]+1});
+            coo.erase(coo.begin());
+            break;
+        case 'r':
+            coo.push_back({coo[coo.size()-1][0]+1,coo[coo.size()-1][1]});
+            coo.erase(coo.begin());
+            break;
+        case 'l':
+            coo.push_back({coo[coo.size()-1][0]-1,coo[coo.size()-1][1]});
+            coo.erase(coo.begin());
+            break;
+        default:
+            break;
+    }
+    
+}
+
+void input()
+{
+    do {
+         switch (getch())
+            {
+                case 65:
+                    if(direction != 'd' && direction != 'u' && not_pause)
+                    {
+                        direction = 'u';
+                    }
+                    break;
+                case 66:
+                    if(direction != 'd' && direction != 'u' && not_pause)
+                    {
+                        direction = 'd';
+                    }
+                    break;
+                case 68:
+                    if(direction != 'l' && direction != 'r' && not_pause)
+                    {
+                        direction = 'l';
+                    }
+                    break;
+                case 67:
+                    if(direction != 'l' && direction != 'r' && not_pause)
+                    {
+                        direction = 'r';
+                    }
+                    break;
+                case 32:
+                    if(run == 0)
+                    {
+                        if(not_pause)
+                        {
+                            not_pause = false;
+                        }
+                        else
+                        {
+                            not_pause = true;
+                        }
+                    }
+                    else if(run == 2)
+                    {
+                        run = 0;
+                        system("clear");
+                        score = 0;
+                    }
+                    break;
+                case 'q':
+                    system("clear");
+                    exit(0);
+                    break;
+                default:
+                    break;
+            }
+	} while(true);
+	 
+}
+
+void check()
+{
+    for (int i=0; i < coo.size(); i++)
+    {
+        if(coo[i][0] < 1 || coo[i][0] >= width-1 || coo[i][1] < 1 || coo[i][1] >= hieght-1)
         {
-            cout << "Score: \t"<< score << endl;
-            cout << "\n\t\tGAME OVER!!\n";
-            run = 2;
+            game_over();
+        }
+    
+        if (coo[i][0] == apple_xy[0] && coo[i][1] == apple_xy[1])
+        {
+            apple_ = true;
+                switch (direction)
+                {
+                    case 'u':
+                        coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]-1});
+                        break;
+                    case 'd':
+                        coo.push_back({coo[coo.size()-1][0],coo[coo.size()-1][1]+1});
+                        break;
+                    case 'r':
+                        coo.push_back({coo[coo.size()-1][0]+1,coo[coo.size()-1][1]});
+                        
+                        break;
+                    case 'l':
+                        coo.push_back({coo[coo.size()-1][0]-1,coo[coo.size()-1][1]});
+                        break;
+                    default:
+                        break;
+                }
+            score+=1;
+        }
+        
+    }
+    for(int p =0; p < coo.size(); p++)
+    {
+        for(int q =0; q < coo.size(); q++)
+        {
+            if(p != q && coo[p][0] == coo[q][0] && coo[p][1] == coo[q][1])
+            {
+                game_over();
+            }
         }
 
-    }while (true);
-     
+    }
+    
+}
 
+void game_over()
+{
+    run = 1;
+    coo.clear();
+    coo.push_back({10,10});
+    snake(length); 
+    direction = 'r';
+    apple_=true;
+    apple_xy.clear();
+    apple_xy[0] = -1;
+    apple_xy[1] = -1;
+    not_pause = false;
+
+}
+
+void snake(int l)
+{
+    for(int i = 0; i < (l-1); i++)
+    {
+        coo.push_back({coo[coo.size()-1][0]+1,coo[coo.size()-1][1]});
+    }
+}
+
+int getch(void) 
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
 }
